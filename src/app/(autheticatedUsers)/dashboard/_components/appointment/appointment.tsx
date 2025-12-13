65bcdfd6db4b7cf982a1dc03ef309300c109911b
aquele best <azullvictorhugo10@gmail.com>
2025-12-12 21:22:37 -0300

    correcting

--- src/app/(autheticatedUsers)/dashboard/_components/appointment/appointment.tsx
+++ src/app/(autheticatedUsers)/dashboard/_components/appointment/appointment.tsx
@@ -1,6 +1,5 @@
 "use server"
 
-import { useSearchParams } from "next/navigation"
 import { GetClinicTimers } from "../../data_acess/get-clinic-timers"
 import { AppointmentContent } from "./appointment-content"
 
